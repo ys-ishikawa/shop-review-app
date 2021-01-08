ec0d881867c5ce27821efb1e8745f8488f9f12e2
yosuke-ishikawa <yosuke1028.ishikawa@gmail.com>
2021-01-08 14:57:01 +0900

    Gitでの管理

--- App.tsx
+++ App.tsx
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+/*lib*/
+import { getShops } from './src/lib/firebase';
+/*components*/
+import { ShopReviewItem } from './src/components/ShopReviewItem';
+/*types*/
+import { Shop } from './src/types/shop';
 
 export default function App() {
+  const [shops, setShops] = useState<Shop[]>([]);
+
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+
+  const getFirebaseItems = async () => {
+    const shops = await getShops();
+    setShops(shops);
+  };
+
+  const shopItems = shops.map((shop, index) => (
+    <ShopReviewItem shop={shop} key={index.toString()} />
+  ));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={shops}
+        renderItem={({ item }: { item: Shop }) => (
+          <ShopReviewItem shop={item} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={2}
+      />
+    </SafeAreaView>
   );
 }
 
---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import { Shop } from '../types/shop';
+import Constants from 'expo-constants';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(Constants.manifest.extra.firebase);
+}
+
+export const getShops = async () => {
+  const snapshot = await firebase
+    .firestore()
+    .collection('shops')
+    .orderBy('score', 'desc')
+    .where('place', '==', '品川')
+    .get();
+  const shops = snapshot.docs.map((doc) => doc.data() as Shop);
+  return shops;
+};

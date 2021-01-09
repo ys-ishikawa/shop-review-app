70718beeea409de9bf48d8c3a59a85b766f33646
yosuke-ishikawa <yosuke1028.ishikawa@gmail.com>
2021-01-09 16:25:06 +0900

    AppNavigatorの導入

--- App.tsx
+++ App.tsx
@@ -1,48 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
-/*lib*/
-import { getShops } from './src/lib/firebase';
-/*components*/
-import { ShopReviewItem } from './src/components/ShopReviewItem';
-/*types*/
-import { Shop } from './src/types/shop';
+import React from 'react';
+import { AppNavigator } from './src/navigation/AppNavigator';
 
 export default function App() {
-  const [shops, setShops] = useState<Shop[]>([]);
-
-  useEffect(() => {
-    getFirebaseItems();
-  }, []);
-
-  const getFirebaseItems = async () => {
-    const shops = await getShops();
-    setShops(shops);
-  };
-
-  const shopItems = shops.map((shop, index) => (
-    <ShopReviewItem shop={shop} key={index.toString()} />
-  ));
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={shops}
-        renderItem={({ item }: { item: Shop }) => (
-          <ShopReviewItem shop={item} />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-        numColumns={2}
-      />
-    </SafeAreaView>
-  );
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
